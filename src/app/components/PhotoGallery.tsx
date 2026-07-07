d6e5edc60c3752f571a26f386e23871068fc2d19
Nayeem An-noor <nayeeman49@googlemail.com>
2026-07-07 18:28:15 +0100

    Replace old photo imports with new ones

--- src/app/components/PhotoGallery.tsx
+++ src/app/components/PhotoGallery.tsx
@@ -36,11 +36,11 @@ import photo23 from "../../imports/mt2.JPG";
 import photo24 from "../../imports/mt3.JPG";
 import photo25 from "../../imports/mt4.JPG";
 import photo26 from "../../imports/mt5.JPG";
-import photo22 from "../../imports/wd1.JPG";
-import photo23 from "../../imports/wd2.JPG";
-import photo24 from "../../imports/wd3.JPG";
-import photo25 from "../../imports/wd4.JPG";
-import photo26 from "../../imports/wd5.JPG";
+import photo27 from "../../imports/wd1.JPG";
+import photo28 from "../../imports/wd2.JPG";
+import photo29 from "../../imports/wd3.JPG";
+import photo30 from "../../imports/wd4.JPG";
+import photo31 from "../../imports/wd5.JPG";
 
 
 export function PhotoGallery() {
@@ -152,27 +152,27 @@ export function PhotoGallery() {
     },
     {
       id: 27,
-      src: photo22, // wd1.JPG
+      src: photo27, // wd1.JPG
       title: "Your Title",
     },
     {
       id: 28,
-      src: photo23, // wd2.JPG
+      src: photo28, // wd2.JPG
       title: "Your Title",
     },
     {
       id: 29,
-      src: photo24, // wd3.JPG
+      src: photo29, // wd3.JPG
       title: "Your Title",
     },
     {
       id: 30,
-      src: photo25, // wd4.JPG
+      src: photo30, // wd4.JPG
       title: "Your Title",
     },
     {
       id: 31,
-      src: photo26, // wd5.JPG
+      src: photo31, // wd5.JPG
       title: "Your Title",
     },
   ];
