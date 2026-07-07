0c72a44001716d5121f490f28c0f500ca1fba147
Nayeem An-noor <nayeeman49@googlemail.com>
2026-07-07 19:28:35 +0100

    Replace documentary thumbnail with new photo

--- src/app/components/VideoSection.tsx
+++ src/app/components/VideoSection.tsx
@@ -9,7 +9,7 @@ import photo19 from '../../imports/P1096539.JPG';
 import photo20 from '../../imports/P1096481.JPG';
 import photo21 from '../../imports/weddingphoto1.JPG';
 import photo22 from '../../imports/mt1.JPG';
-
+import photo23 from '../../imports/tokyo.JPG';
 
 export function VideoSection() {
   const videos = [
@@ -30,7 +30,7 @@ export function VideoSection() {
     {
       id: 3,
       title: 'Documentaries and Vlogs',
-      thumbnail: documentaryThumbnail,
+      thumbnail: photo23,
       duration: '5:20',
       videoUrl: 'https://www.youtube.com/watch?v=fr2C4qPumRQ',
     },
